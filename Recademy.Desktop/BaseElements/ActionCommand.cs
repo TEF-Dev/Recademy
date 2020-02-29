3af3b527ca3e8141733115da20227129342a0078
Alexey I.m2strng4dtwrld <inredikawb@gmail.com>
2020-02-29 20:28:18 +0300

    add MVVM common code

---
+++ Recademy.Desktop/BaseElements/ActionCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Recademy.Desktop.BaseElements
+{
+    public class ActionCommand : ICommand
+    {
+        private readonly Action _action;
+        private readonly Func<bool> _isCanExec;
+
+        public ActionCommand(Action action, Func<bool> isCanExec = null)
+        {
+            _action = action;
+            _isCanExec = isCanExec;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _isCanExec == null || _isCanExec();
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter != null)
+                throw new ArgumentException();
+            _action();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
---
+++ Recademy.Desktop/BaseElements/BaseNotificationClass.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Recademy.Desktop.BaseElements
+{
+    public class BaseNotificationClass : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected bool SetField<T>(ref T field, T value, string propertyName)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
